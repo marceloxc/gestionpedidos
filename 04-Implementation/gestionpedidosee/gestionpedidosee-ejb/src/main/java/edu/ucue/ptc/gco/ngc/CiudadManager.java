25b4de8c9f98270786681f6fc7bd60bba24c2c92
DELL <DELL@192.168.1.106>
2013-05-16 22:36:18 -0500

    servicios web

---
+++ 04-Implementation/gestionpedidosee/gestionpedidosee-ejb/src/main/java/edu/ucue/ptc/gco/ngc/CiudadManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ucue.ptc.gco.ngc;
+
+/**
+ *
+ * @author DELL
+ */
+public interface CiudadManager {
+    
+}
